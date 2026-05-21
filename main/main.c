348595a75a80941bb1470e931ade8d8fda5619f5
josesalas7 <josesalasdm@gmail.com>
2026-05-21 17:37:47 -0300

    ajuste

--- main/main.c
+++ main/main.c
@@ -253,7 +253,7 @@ void stats_task(void *p) {
 #endif
 
 /* Chamado pelo FreeRTOS ao detectar estouro de stack (configCHECK_FOR_STACK_OVERFLOW=2) */
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
+void vApplicationStackOverflowHook(TaskHandle_t xTask, const char *pcTaskName) {
     (void)xTask;
     printf("STACK OVERFLOW: %s\n", pcTaskName);
     for (;;);
